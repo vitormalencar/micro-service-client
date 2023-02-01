1779f5468bb5867b9c35f6a0024aeb4ad4eff18e
vitormalencar <vitor.maia.alencar@gmail.com>
2023-02-01 17:22:04 +0100

    chore: add video id to file upload

--- src/features/videos/VideosCreate.tsx
+++ src/features/videos/VideosCreate.tsx
@@ -1,8 +1,11 @@
 import { Box, Paper, Typography } from "@mui/material";
+import { nanoid } from "nanoid";
 import { useSnackbar } from "notistack";
 import { useEffect, useState } from "react";
+import { useAppDispatch } from "../../app/hooks";
 import { useUniqueCategories } from "../../hooks/useUniqueCategories";
-import { Video } from "../../types/Videos";
+import { FileObject, Video } from "../../types/Videos";
+import { addUpload } from "../uploads/UploadSlice";
 import { VideosForm } from "./components/VideosForm";
 import { mapVideoToForm } from "./util";
 import {
@@ -19,32 +22,37 @@ export const VideosCreate = () => {
   const [createVideo, status] = useCreateVideoMutation();
   const [videoState, setVideoState] = useState<Video>(initialState);
   const [caregories] = useUniqueCategories(videoState, setVideoState);
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-
-  console.log("selectedFiles", selectedFiles);
+  const [selectedFiles, setSelectedFiles] = useState<FileObject[]>([]);
+  const dispatch = useAppDispatch();
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
     setVideoState((state) => ({ ...state, [name]: value }));
   }
 
-  function handleAddFile(files: FileList | null) {
-    if (!files) return;
-    const filesArr = Array.from(files);
-    setSelectedFiles([...selectedFiles, ...filesArr]);
+  function handleAddFile({ name, file }: FileObject) {
+    setSelectedFiles([...selectedFiles, { name, file }]);
   }
 
-  function handleRemoveFile(file: File) {
-    setSelectedFiles(selectedFiles.filter((f) => f !== file));
+  function handleRemoveFile(name: string) {
+    setSelectedFiles(selectedFiles.filter((file) => file.name !== name));
+  }
+
+  function handleSubmitUploads(videoId: string) {
+    selectedFiles.forEach(({ file, name }) => {
+      const payload = { id: nanoid(), file, videoId, field: name };
+      dispatch(addUpload(payload));
+    });
   }
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const { id, ...payload } = mapVideoToForm(videoState);
     try {
-      await createVideo(payload);
+      const { data } = await createVideo(payload).unwrap();
+      handleSubmitUploads(data.id);
     } catch (e) {
-      console.log(e);
+      enqueueSnackbar(`Error creating Video`, { variant: "error" });
     }
   }
 
@@ -69,10 +77,10 @@ export const VideosCreate = () => {
 
         <VideosForm
           video={videoState}
+          categories={caregories}
           genres={genres?.data}
           isLoading={status.isLoading}
           isDisabled={status.isLoading}
-          categories={caregories}
           castMembers={castMembers?.data}
           handleChange={handleChange}
           handleSubmit={handleSubmit}
